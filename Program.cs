2379148e6da90a8aaa47af0f7feaa41e3a88f255
CleanIce <CleanIce2022@163.com>
2023-08-15 11:34:58 +0800

    Init git

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Program {
+    public class Program {
+        public static void Main() {
+            const int MAXN = 40005;
+            long[] arr = new long[MAXN];
+            long ans = 2;
+            long n = int.Parse(Console.ReadLine() ?? throw new Exception("NULL"));
+            if (n == 1) {
+                Console.WriteLine(0);
+                return;
+            }
+            for (int i = 2; i <= n; i++) {
+                arr[i] = i;
+            }
+            for (int i = 2; i <= n; i++) {
+                if (arr[i] == i) {
+                    for (int j = i; j <= n; j += i) {
+                        arr[j] = arr[j] / i * (i - 1);
+                    }
+                }
+            }
+            n--;
+            for (int i = 2; i <= n; i++) {
+                ans += arr[i] * 2;
+            }
+            Console.WriteLine(ans + 1);
+        }
+    }
+}
